604ea3527cee54ee0e090868564cd5a26c73d324
BeamlakTesfahun <111596136+BeamlakTesfahun@users.noreply.github.com>
2022-10-09 05:25:59 -0600

    Create Swap_Case.py

---
+++ Swap_Case.py
@@ -0,0 +1,11 @@
+def swap_case(s):
+    empty = ""
+    for chara in s:
+        if chara.islower():
+            chara = chara.upper()
+        else:
+            chara = chara.lower()
+        empty += "".join(chara)
+    return empty
+
+if __name__ == '__main__':
